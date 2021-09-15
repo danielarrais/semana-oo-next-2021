f437eb6403eaf9365d5a0d58b835a8fb3a81ca90
Daniel Carvalho <danielarraiscarvalho@gmail.com>
2021-09-15 20:13:49 -0300

    Cria representação de transação

---
+++ src/aula02/contaBancaria/Transacao.java
@@ -0,0 +1,40 @@
+package aula02.contaBancaria;
+
+import java.util.Date;
+
+// Representação de uma transação (saque, depósito...)
+public class Transacao {
+    private Double valor;
+    private Date data;
+    private String tipo;
+
+    public Transacao(Double valor, String tipo) {
+        this.valor = valor;
+        this.data = new Date();
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return this.data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
