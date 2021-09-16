a7ac995c45b48464870cefc5e2c651fe57aefd2e
Daniel Carvalho <danielarraiscarvalho@gmail.com>
2021-09-16 20:36:51 -0300

    Cria uma representação de conta sobrescrevendo comportamento

---
+++ src/aula03/contaBancaria/ContaCorrente.java
@@ -0,0 +1,16 @@
+package aula03.contaBancaria;
+
+public class ContaCorrente extends Conta{
+
+    private Double limite; // cheque especial
+
+    public ContaCorrente(String numero, String agencia, Double limite, Double saldo) {
+        super(numero, agencia, saldo);
+        this.limite = limite;
+    }
+
+    @Override
+    protected boolean podeSacar(Double saque) {
+        return super.getSaldo() != null && saque <= (super.getSaldo() + limite);
+    }
+}
