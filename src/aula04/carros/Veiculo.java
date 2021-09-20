0f637be16c1389d8bf813ec4e3a8c71d7d917940
Daniel Carvalho <danielarraiscarvalho@gmail.com>
2021-09-20 20:32:47 -0300

    Cria um veiculo

---
+++ src/aula04/carros/Veiculo.java
@@ -0,0 +1,115 @@
+package aula04.carros;
+
+public class Veiculo {
+    private String marca;
+    private String cor;
+    private String placa;
+    private Integer ano;
+    private Integer velocidadeAtual;
+    private Integer velocidadeMaxima;
+    private Boolean ligado;
+
+    public Veiculo(
+        String marca, 
+        String cor, 
+        String placa, 
+        Integer ano, 
+        Boolean ligado
+    ) {
+        this.marca = marca;
+        this.cor = cor;
+        this.placa = placa;
+        this.ano = ano;
+        this.ligado = ligado;
+    }
+
+    public void acelerar(Integer velocidade) {
+        if(velocidade <= velocidadeMaxima) {
+            this.velocidadeAtual = velocidade;
+            return;
+        }
+        
+        this.velocidadeAtual = velocidadeMaxima;
+    }
+
+    public void frear() {
+        this.velocidadeAtual = 0;
+    }
+
+    public void comando(String comando) {
+        if(comando == "LIGAR") {
+            this.ligado = true;
+        } else if(comando == "DESLIGAR") {
+            this.ligado = false;
+        }
+    }
+
+    public void ligar() {
+        this.ligado = true;
+    }
+
+    public void desligar() {
+        this.ligado = false;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return this.placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Integer getAno() {
+        return this.ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getVelocidadeAtual() {
+        return this.velocidadeAtual;
+    }
+
+    public void setVelocidadeAtual(Integer velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public Integer getVelocidadeMaxima() {
+        return this.velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(Integer velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public Boolean isLigado() {
+        return this.ligado;
+    }
+
+    public Boolean getLigado() {
+        return this.ligado;
+    }
+
+    public void setLigado(Boolean ligado) {
+        this.ligado = ligado;
+    }
+
+}
